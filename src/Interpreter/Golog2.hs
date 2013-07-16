8d99271bc6042201fb8be08df7d53f2059ddbf20
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2013-07-17 01:18:43 +0200

    Restructured transition semantics (mostly untested).

--- src/Interpreter/Golog2.hs
+++ src/Interpreter/Golog2.hs
@@ -43,6 +43,7 @@ data Tree a where
    Empty :: Tree a
    Alt   :: [Tree a] -> Tree a
    Val   :: a -> Tree a -> Tree a
+   deriving Show
 
 instance Monoid (Tree a) where
    mempty                = Empty
@@ -60,11 +61,17 @@ scanl _ x Empty     = Val x Empty
 scanl f x (Val y t) = Val x (scanl f (f x y) t)
 scanl f x (Alt ts)  = Alt (map (scanl f x) ts)
 
-best :: a -> (a -> a -> Ordering) -> (a -> Tree a -> Bool) -> Depth -> Tree a -> a
+resolve :: ([Tree a] -> Tree a) -> Tree a -> Tree a
+resolve _ Empty     = Empty
+resolve f (Val x t) = Val x (resolve f t)
+resolve _ (Alt [])  = Empty
+resolve f (Alt ts)  = f ts
+
+best :: a -> (a -> a -> Ordering) -> (Tree a -> Bool) -> Depth -> Tree a -> a
 best def _   _   _ Empty                 = def
 best def cmp cut l (Alt ts)              = maximumBy cmp (map (best def cmp cut l) ts)
 best def cmp cut l (Val x t) | l == 0    = x
-                             | cut x t   = maximumBy cmp [x, best def cmp cut (l-1) t]
+                             | cut t     = maximumBy cmp [x, best def cmp cut (l-1) t]
                              | l > 0     = best def cmp cut (l-1) t
                              | otherwise = error "best: l < 0"
 
@@ -95,42 +102,84 @@ den p' = rec (den' p')
          den' (PseudoAtom a) = Val a Empty
          den' Nil            = Empty
 
-trans :: BAT a => Sit a -> Tree (Atom a) -> [(Sit a, Tree (Atom a))]
-trans _ Empty                        = []
-trans s (Alt ts)                     = concat $ map (\t -> trans s t) ts
-trans s (Val (Prim a)  t) | poss a s = [(do_ a s, t)]
-trans s (Val (PrimF a) t)            = trans s (Val (Prim (a s)) t)
-trans s (Val (Test f)  t) | f s      = [(s, t)]
-trans _ (Val _         _)            = []
+data Conf a b = Conf (Tree b) (Sit a)
 
-treeDT :: DTBAT a => Sit a -> Prog a -> Tree (Maybe (Sit a, Reward, Depth))
-treeDT sz p = scanl augment root (den p)
-   where root = Just (sz, 0, 0)
-         augment :: DTBAT a => Maybe (Sit a, Reward, Depth) -> Atom a -> Maybe (Sit a, Reward, Depth)
-         augment (Just (s,r,d)) (Prim a)  | poss a s = Just (do_ a s, r + reward a s, d + 1)
-         augment (Just (s,r,d)) (PrimF a)            = augment (Just (s,r,d)) (Prim (a s))
-         augment (Just (s,r,d)) (Test f)  | f s      = Just (s, r, d + 1)
-         augment _              _                    = Nothing
+instance Show b => Show (Conf a b) where
+   show (Conf t s) = show t
 
-transDT :: DTBAT a => Sit a -> Tree (Sit a, Reward, Depth) -> Maybe (Sit a, Tree (Sit a, Reward, Depth))
-transDT _ Empty           = Nothing
-transDT _ (Val (s,_,_) t) = Just (s, t)
---transDT _ (Alt [])        = Nothing
-transDT s (Alt ts)        = maximumBy (comparing (value (lookahead s))) ts
-   where value :: DTBAT a => Depth -> Tree (Maybe (Sit a, Reward, Depth)) -> (Reward, Depth)
-         value l t = val (best def cmp fnl l t)
-            where val :: (Num b, Num c, Ord b, Ord c) => Maybe (a, b, c) -> (b, c)
-                  val (Just (_, r, d)) = (r, d)
-                  val Nothing          = (0, 0)
-                  def             = Just (s0, 0, 0)
-                  fnl _ Empty     = True
-                  fnl _ (Alt ts)  = any (\t -> case t of Empty -> True ; _ -> False) ts
-                  fnl _ (Val _ _) = False
-                  cmp :: (Num b, Num c, Ord b, Ord c) => Maybe (a, b, c) -> Maybe (a, b, c) -> Ordering
-                  cmp x y         = compare (val x) (val y)
+type NodeN a = Maybe (Sit a)
+type NodeDT a = Maybe (Sit a, Reward, Depth)
 
+tree :: BAT a => Prog a -> Sit a -> Conf a (NodeN a)
+tree p sz = Conf (scanl exec (Just sz) (den p)) sz
+   where exec :: BAT a => NodeN a -> Atom a -> NodeN a
+         exec (Just s) (Prim a)  | poss a s = Just (do_ a s)
+         exec (Just s) (PrimF a)            = exec (Just s) (Prim (a s))
+         exec (Just s) (Test f)  | f s      = Just s
+         exec _        _                    = Nothing
+
+treeDT :: DTBAT a => Prog a -> Sit a -> Conf a (NodeDT a)
+treeDT p sz = Conf (resolve choice (scanl exec (Just (sz,0,0)) (den p))) sz
+   where exec :: DTBAT a => NodeDT a -> Atom a -> NodeDT a
+         exec (Just (s,r,d))   (Prim a)  | poss a s = let s' = do_ a s
+                                                          r' = reward a s
+                                                          d' = d+1
+                                                      in Just (s',r',d')
+         exec c@(Just (s,_,_)) (PrimF a)            = exec c (Prim (a s))
+         exec (Just (s,r,d))   (Test f)  | f s      = Just (s,r,d+1)
+         exec _                _                    = Nothing
+         choice = maximumBy (comparing (value (lookahead sz)))
+         value :: DTBAT a => Depth -> Tree (NodeDT a) -> (Reward, Depth)
+         value l t = val $ best def cmp final' l t
+            where def = Just (s0,0,0)
+                  val (Just (_,r,d)) = (r,d)
+                  val Nothing        = (0,0)
+                  cmp x y = compare (val x) (val y)
+
+trans :: BAT a => Conf a (NodeN a) -> [Conf a (NodeN a)]
+trans (Conf Empty             _) = []
+trans (Conf (Val Nothing   _) _) = []
+trans (Conf (Val (Just s') t) _) = [Conf t s']
+trans (Conf (Alt ts)          s) = concat $ map (\t -> trans (Conf t s)) ts
+
+transDT :: BAT a => Conf a (NodeDT a) -> [Conf a (NodeDT a)]
+transDT (Conf Empty             _)       = []
+transDT (Conf (Val Nothing   _) _)       = []
+transDT (Conf (Val (Just (s',_,_)) t) _) = [Conf t s']
+transDT (Conf (Alt ts)          s)       = concat $ map (\t -> transDT (Conf t s)) ts
+
+trans' :: BAT a => Conf a (NodeN a) -> Maybe (Conf a (NodeN a))
+trans' c = case trans c of []   -> Nothing
+                           c':_ -> Just c'
+
+final  :: Conf a b -> Bool
+final (Conf t _) = final' t
+
+final' :: Tree a -> Bool
+final' Empty     = True
+final' (Alt [])  = True
+final' (Alt ts)  = any final' ts
+final' (Val _ _) = False
+
+{-
 exec :: BAT a => a -> State (Maybe (Sit a)) ()
 exec a = state f
    where f (Just s) | poss a s = ((), Just (do_ a s))
          f _                   = ((), Nothing)
+-}
+
+instance BAT Int where
+   data Sit Int = S0 | Do Int (Sit Int) deriving Show
+   s0 = S0
+   do_ = Do
+   poss a _ = even a
+
+instance DTBAT Int where
+   reward a _ = fromIntegral a
+   lookahead _ = 2
+
+p = PseudoAtom . Atom . Prim
+q = PseudoAtom . Complex
+
+-- let pp = Nondet [p 2 `Seq` p 4, p 6, p 0 `Seq` p 0 `Seq` p 10] :: Prog Int ; cc = treeDT pp s0 ; sit (Conf _ s) = s in mapM_ (putStrLn.show) $ take 90 $ map sit $ concat $ iterate (concat . map transDT) [cc]
 
