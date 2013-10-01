7b87787f4af5f70baa7e7e4f2a2924440e6ba27f
Christoph Schwering <schwering@kbsg.rwth-aachen.de>
2013-10-01 19:41:31 +0200

    Golog Examples: profiling for basic elevator.

--- golog-examples/elevator-basic/ElevatorBasic.hs
+++ golog-examples/elevator-basic/ElevatorBasic.hs
@@ -16,6 +16,7 @@ module Main where
 
 import Prelude hiding (floor, until)
 import Data.List (delete)
+import Data.Time.Clock
 import Golog.Interpreter
 import Golog.Macro
 import Golog.Util
@@ -24,7 +25,7 @@ import Golog.Util
 type Floor = Int
 
 floors :: [Floor]
-floors = [0..10]
+floors = [0..2001]
 
 -- There are three actions: Up and Down move the elevator to a floor,
 -- TurnOff switches off the call-button. Test is a test-action, because
@@ -42,7 +43,7 @@ instance TestAction A where
 instance BAT A where
    data Sit A = State { floor :: Floor, onButtons :: [Floor] }
 
-   s0 = State { floor = 7, onButtons = [3, 5] }
+   s0 = State { floor = 7, onButtons = take 1000 [3,5..] }
 
    do_ (Up n)      s = s{floor = n}
    do_ (Down n)    s = s{floor = n}
@@ -57,18 +58,29 @@ instance BAT A where
 on :: Floor -> Sit A -> Bool
 on n s = n `elem` onButtons s
 
-goFloor :: Floor -> Prog A
-goFloor n = choice [ prim (Up n), test (\s -> floor s == n), prim (Down n) ]
+goFloor :: (Sit A -> Floor) -> Prog A
+goFloor n = choice [ primf (Up . n)
+                   , test (\s -> floor s == n s)
+                   , primf (Down . n) ]
 
 control :: Prog A
 control = until (null.onButtons)
-                (pick floors (\n -> test (on n) `Seq`
-                                    goFloor n `Seq`
-                                    prim (TurnOff n)))
+                (pickInf [0..] (\i -> test (\s -> i < length (onButtons s)) `Seq`
+                                      goFloor (n i) `Seq`
+                                      primf (TurnOff . n i))) `Seq`
+          goFloor (const 1)
+   where n i s = onButtons s !! i
 
 -- The IOBAT typeclass is for defining real world effects of actions. We use
 -- this here to just print the state.
 instance IOBAT A IO where
+   syncA a s = putStr as >> putChar ' ' >> return s'
+      where s' = do_ a s
+            as = case a of Up      n -> "Up "++ show n
+                           Down    n -> "Down "++ show n
+                           TurnOff n -> "TurnOff "++ show n
+                           Test _    -> ""
+{-
    syncA a s = maybe (return s') (\str -> putStrLn str >> return s') as
       where s' = do_ a s
             as = case a of Up      n -> Just $ "Up "++ show n ++":      "++ info
@@ -76,9 +88,15 @@ instance IOBAT A IO where
                            TurnOff n -> Just $ "TurnOff "++ show n ++": "++ info
                            Test _    -> Nothing
             info = "Floor = "++ show (floor s') ++", On-buttons = "++ show (onButtons s')
+-}
 
 main :: IO ()
-main = do let Just c1 = doo' (treeNDIO control s0)
-          _ <- sync c1 -- prints all states
+main = do t1 <- getCurrentTime
+          Just c1 <- dooIO (const (Offline DFS)) (treeNDIO control s0)
+          t2 <- getCurrentTime
+          putStrLn ""
+          putStrLn $ show (diffUTCTime t2 t1)
+          --let Just c1 = doo' (treeNDIO control s0)
+          --_ <- sync c1 -- prints all states
           return ()
 
